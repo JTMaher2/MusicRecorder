c17f02cec12304213eebdafb68ed424d9566238e
James <jtmaher2@gmail.com>
2022-02-20 12:30:48 -0600

    updated Nuget packages, and updated Syncfusion license

--- MusicRecorderUWP/Properties/AssemblyInfo.cs
+++ MusicRecorderUWP/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright 2022 James Maher
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("James Maher")]
 [assembly: AssemblyProduct("MusicRecorderUWP")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ?  2022")]
 [assembly: AssemblyTrademark("A place for music.")]
 [assembly: AssemblyCulture("")]
 
@@ -41,7 +41,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.7.0")]
-[assembly: AssemblyFileVersion("1.0.7.0")]
+[assembly: AssemblyVersion("1.0.8.0")]
+[assembly: AssemblyFileVersion("1.0.8.0")]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
